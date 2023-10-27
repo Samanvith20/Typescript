0593a1fb48c08de0b61033fc65e9dfea5fe2a470
Samanvith <Samanvith2005@gmail.com>
2023-10-27 23:51:45 +0530

    updated Variable

--- 02Variable.js
+++ 02Variable.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
 var greetings = "Hello Samanvith";
 console.log(greetings);
+// number
+var myNumber = 36472.35;
+//boolean
+var isloggedIn = true;
--- 02Variable.ts
+++ 02Variable.ts
@@ -1,2 +1,8 @@
+// string
 let greetings:string = "Hello Samanvith"
 console.log(greetings)
+// number
+let myNumber:number=36472.35
+//boolean
+let isloggedIn:boolean=true
+ export {}
