972de175d9ee5b4c28a96a6760cfdff0c3da1fe5
Samanvith <Samanvith2005@gmail.com>
2023-10-28 10:19:25 +0530

    Added Functions

---
+++ 03myfunction.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// number
+function addtwo(num) {
+    return num + 2;
+}
+addtwo(3);
+// string
+function touppercase(val) {
+    return val.toUpperCase();
+}
+touppercase("samanvith");
+//boolean
+function signupuser(name, email, password) { }
+signupuser(56374, "sam@435", true);
+var heros = ["ironman", "thor", "superheros"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+// Note: we dont use any type  while declaring the function defintely we should have to declare any variable while using functions in the project
+exports.default = {};
---
+++ 03myfunction.ts
@@ -0,0 +1,19 @@
+ // number
+ function addtwo(num:number){
+   return num+2;
+}
+addtwo(3)
+// string
+function touppercase(val:string){
+  return val.toUpperCase()
+}
+touppercase("samanvith")
+//boolean
+function signupuser(name:number,email:string,password:boolean){}
+signupuser(56374 ,"sam@435",true)
+const heros= ["ironman","thor","superheros"]
+heros.map((hero):string=>{
+   return `hero is ${hero}`
+})
+// Note: we dont use any type  while declaring the function defintely we should have to declare any variable while using functions in the project
+ export default {}
